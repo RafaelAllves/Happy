8fd7c2c52464c28d3f790c1b722006c802eb848e
Rafael Alves <rafael.allves91@gmail.com>
2021-10-12 19:26:44 -0300

    orphanage model created

--- backend/src/database/migrations/1627169971692-create_orphanages.ts
+++ backend/src/database/migrations/1627169971692-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1627169971692 implements MigrationInterface {
           type: 'text',
         },
         {
+          name: 'opening_hours',
+          type: 'varchar',
+        },
+        {
           name: 'open_on_weekends',
           type: 'boolean',
           default: false
