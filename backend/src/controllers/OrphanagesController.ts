2a8f5b4f47d8e46b4cce506fb9428b852fd07d97
Rafael Alves <rafael.allves91@gmail.com>
2021-10-12 20:37:34 -0300

    route to list orphanages

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,9 +1,18 @@
 import { Request,  Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, Index } from 'typeorm';
 
 import Orphanage from '../models/Orphanages';
 
 export default {
+
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+
+  },
+
   async create(request: Request, response: Response) {
     const {
       name,
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,7 @@ import OrphanagesController from './controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index)
 routes.post('/orphanages', OrphanagesController.create)
 
 export default routes;
