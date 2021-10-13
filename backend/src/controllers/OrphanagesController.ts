27f8ede10418c509dd53f57489a449fd2a2311d6
Rafael Alves <rafael.allves91@gmail.com>
2021-10-12 22:48:06 -0300

    data validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,5 +1,6 @@
 import { Request,  Response } from 'express';
 import { getRepository, Index } from 'typeorm';
+import * as Yup from 'yup';
 
 import Orphanage from '../models/Orphanage';
 import orphanagesView from '../views/orphanages_view';
@@ -47,9 +48,7 @@ export default {
       return { path: image.filename}
     })
     
-
-  
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -58,7 +57,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
-    });
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        }),
+      ),
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+  
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
