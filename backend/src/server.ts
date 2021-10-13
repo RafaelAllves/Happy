3f800292771040d1d4780ab4476005613340e2da
Rafael Alves <rafael.allves91@gmail.com>
2021-10-12 22:27:53 -0300

    dealing with errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express, { request, response } from 'express';
+import 'express-async-errors';
 import path from 'path';
 
 import './database/connection';
 import routes from './routes';
+import errorHandler from './erros/handler';
 
 
 const app = express();
 
 app.use(express.json());
-app.use(routes)
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
-app.listen(3333)
+app.listen(process.env.PORT)
